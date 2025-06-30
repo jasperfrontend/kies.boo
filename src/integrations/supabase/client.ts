3606bba0a541e04aea52148baaced8e2bdce6132
Jasper van der Meer <jaspervdmeer@gmail.com>
2025-06-30 23:38:59 +0200

    removed... things

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://efaaigzvqhgoozvdkhzo.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmYWFpZ3p2cWhnb296dmRraHpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNTUxNjksImV4cCI6MjA2NjYzMTE2OX0.TZqfgBFd-KqnrUSqVkfQTUz8lZHvdgGP-xrLl5HzxqI";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
