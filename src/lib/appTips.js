b188847f2195dad7e0886ac21831c15c1240550a
Jasper van der Meer <jaspervdmeer@gmail.com>
2025-07-08 20:45:45 +0200

    Fixed odd spelling in `appTips.js`

--- src/lib/appTips.js
+++ src/lib/appTips.js
@@ -6,7 +6,7 @@ const appTips = [
   "Click the edit icon on the far right of the table to edit a bookmark",
   "Hit Space to select a row - or use the checkbox to select a row",
   "Use Alt+i after selecting 1 or more rows to quickly delete the selected bookmark(s).",
-  "You can undo a delete action bookmarks by pressing Alt+U within 10 seconds of deletion.",
+  "You can undo a 'delete bookmarks' action by pressing Alt+u within 10 seconds of deletion.",
 ];
 
 export default appTips;
